39f6792558e2e17202ec9404bc7e1a46a4690724
Alexey <astapchiktranslate@gmail.com>
2020-03-04 18:40:45 +0300

    l1

---
+++ L1/code.js
@@ -0,0 +1,50 @@
+// first task
+
+// let a = "first string";
+
+// alert(a);
+
+// a = "changed string";
+// console.log(a);
+
+// second task
+
+// let num = 100;
+
+// let str = "Hello";
+
+// let bool = true;
+
+// let user = {
+//     name: 'John',
+//     age: '23'
+// };
+
+// let car; 
+
+
+// let f = null;
+
+// alert(f);
+// alert(str);
+// alert(bool);
+// alert(user);
+// alert(car);
+
+// console.log(f);
+// console.log(str);
+// console.log(bool);
+// console.log(user);
+// console.log(car);
+
+
+// third task
+
+// let age = prompt('How old are you?');
+
+// if (age >= 18){
+//     alert('Congrats! You are of age!');
+// }
+// else {
+//     alert('You are only ' + age + ' years old. You may not go!')
+// }
